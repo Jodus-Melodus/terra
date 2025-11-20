0ad2f3b121d7089610ff632ff7b9ad057a1b82ba
JodusMelodus <143637507+Jodus-Melodus@users.noreply.github.com>
2025-11-20 19:45:01 +0200

    update cmake and basic window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 
+#include "raylib.h"
+#include "screen.h"
+
 int main()
 {
-    printf("Hello world");
+    const unsigned int width = 1700;
+    const unsigned int height = 960;
+
+    InitWindow(width, height, "Terra");
+    SetTargetFPS(60);
+
+    ScreenBuffer *screen = CreateScreenBuffer(width, height);
+
+    Image image = {
+        .data = screen->layers[0]->buffer,
+        .width = width,
+        .height = height,
+        .format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8,
+        .mipmaps = 1};
+
+    Texture2D texture = LoadTextureFromImage(image);
+
+    while (!WindowShouldClose())
+    {
+        UpdateTexture(texture, screen->layers[0]->buffer);
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawTexture(texture, 0, 0, WHITE);
+        EndDrawing();
+    }
+
+    UnloadTexture(texture);
+    FreeScreenBuffer(screen);
+    CloseWindow();
     return 0;
 }
